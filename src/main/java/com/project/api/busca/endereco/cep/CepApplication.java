4ee2f005a299730fb424eadd39d454f24f014e45
arielbat19 <arielbatista853@gmail.com>
2023-02-05 12:44:20 -0300

    Criação da documentação

--- src/main/java/com/project/api/busca/endereco/cep/CepApplication.java
+++ src/main/java/com/project/api/busca/endereco/cep/CepApplication.java
@@ -3,7 +3,10 @@ package com.project.api.busca.endereco.cep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class CepApplication {
 
 	public static void main(String[] args) {
